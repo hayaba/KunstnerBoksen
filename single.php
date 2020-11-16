5b5ae7e20b061552100407fb831603728e2c34d5
Haya Barakat <56253728+hayaba@users.noreply.github.com>
2020-11-16 19:29:16 +0100

    First commit

---
+++ single.php
@@ -0,0 +1,1 @@
+<!-- To control single post -->
---
+++ special-template.php
@@ -0,0 +1,33 @@
+<!--  Here we can add elements to add for multiple pages -->
+
+<?php
+
+/* 
+Template Name: Special Layout
+*/
+
+get_header();
+
+if (have_posts()):
+    while (have_posts()) : the_post(); ?>
+    <article class="post page">
+        <h2><?php the_title(); ?></h2>
+
+        <!-- info-box -->   
+        <div class="info-box">
+            <h4>Disclamer Title</h4>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+</p>
+        </div>
+        <!-- /info-box -->
+
+        <?php the_content(); ?>
+    </article>
+<?php endwhile;
+else: 
+    echo '<p> No content found </p> ';
+
+endif;
+
+get_footer();
+?>
